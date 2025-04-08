f5d624bdef570095d4b5f2a49901d63f4eafd40f
Abdul-Hadi-Jalil <abdulhadijalil34@gmail.com>
2025-04-08 18:51:03 +0500

    started the login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/screens/signup.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
@@ -9,59 +8,29 @@ class Login extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Center(
-            child: Text(
-              'Login',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
+          Text("Welcome back"),
+          Text("Email"),
+          Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+            child: TextFormField(keyboardType: TextInputType.emailAddress),
           ),
-          Padding(
-            padding: const EdgeInsets.only(
-              left: 200,
-              right: 200,
-              top: 50,
-              bottom: 20,
-            ),
-            child: Column(
-              children: [
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Username',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                  ),
-                ),
-                ElevatedButton(onPressed: () {}, child: Text('Login')),
-                TextButton(onPressed: () {}, child: Text('Forgot Password')),
-              ],
-            ),
+          Text("Password"),
+          Container(
+            decoration: BoxDecoration(border: Border.all(color: Colors.black)),
+            child: TextFormField(),
           ),
-          Center(
-            child: Row(
-              spacing: 10,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('Not a member?'),
-                InkWell(
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Signup()),
-                    );
-                  },
-                  hoverColor: Colors.transparent,
-                  focusColor: Colors.transparent,
-                  splashColor: Colors.transparent,
-                  highlightColor: Colors.transparent,
-                  child: Text('Sign up now'),
-                ),
-              ],
-            ),
+          InkWell(
+            onTap: () {
+              print('it is pressed');
+            },
+            child: Text('Forgot password?'),
+          ),
+          // Login button for user to login to the app
+          ElevatedButton(
+            onPressed: () {
+              print("Login button is pressed");
+            },
+            child: Text('Login'),
           ),
         ],
       ),
